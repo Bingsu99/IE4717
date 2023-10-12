7b5b92936cb44725d5b7624a9dddafc01c1689f8
zhixinnnzx <185545x@gmail.com>
2023-10-12 09:37:58 +0800

    updated w/ require

---
+++ CaseStudy2/jobs_validator.js
@@ -0,0 +1,9 @@
+// JavaScript RegExp Reference
+// https://www.w3schools.com/jsref/jsref_regexp_charset.asp
+// event listener
+// https://medium.com/@ysmiracle/use-addeventlistener-instead-of-onclick-oninput-onchange-especially-when-working-in-teams-50ad40badb8d
+
+function chkName(event) {}
+function chkEmail(event) {}
+function chkStartDate(event) {}
+function chkExp(event) {}
