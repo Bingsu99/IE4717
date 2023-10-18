71e565842a50971930fa194196f3026a87d43aab
zhixinnnzx <185545x@gmail.com>
2023-10-18 23:45:15 +0800

    job validation-done

--- CaseStudy2/js/jobs_validator.js
+++ CaseStudy2/js/jobs_validator.js
@@ -1,27 +1,13 @@
-// var customerNode = document.getElementById("custName");
-// var phoneNode = document.getElementById("phone");
-// customerNode.addEventListener("change", chkName, false);
-// phoneNode.addEventListener("change", chkPhone, false);
-
-// var Name = document.getElementById("name");
-// var Email = document.getElementById("email");
-// var startDate = document.getElementById("date");
-// // var Experience = document.getElementById("experience");
-
-// Name.addEventListener("change", chkName, false);
-// Email.addEventListener("change", chkEmail, false);
-// startDate.addEventListener("change", chkStartDate, false);
-// // Experience.addEventListener("change", chkExp, false);
-
 // checking name
 function chkName() {
   var name = document.getElementById("name").value;
-  name.trim(); //remove any whitespace from both ends of the string
+  //remove any whitespace from both ends of the string
+  name.trim();
+
   if (name.length > 0) {
     // make sure it is not empty
     var regexp = /^([A-z',.\s?]+)$/;
     if (regexp.test(name)) {
-      alert("good.");
       return true;
     } else {
       alert(
@@ -38,17 +24,18 @@ function chkName() {
 function chkEmail() {
   var email = document.getElementById("email").value;
   email.trim();
+
+  //make sure it is not empty
   if (email.length > 0) {
-    //make sure it is not empty
-    var regexp = /^([\w\.-])+@([\w]+\.){1,3}([A-z]){2,3}$/;
+    var regexp = /^([\w\.-])+@([\w]+\.){1,4}([A-z]){2,3}$/;
     if (regexp.test(email)) {
-      alert("amazing.");
       return true;
     } else {
       alert("Email entered in wrong format.");
       return false;
     }
   }
+
   alert("Please fill in your email." + email);
   return false;
 }
@@ -57,31 +44,31 @@ function chkEmail() {
 function chkStartDate() {
   var date = new Date(document.getElementById("date").value);
   var currentDate = new Date();
+
+  // check year
   if (date.getFullYear() > currentDate.getFullYear()) {
-    alert("can.");
     return true;
   } else if (date.getFullYear() == currentDate.getFullYear()) {
+    // check month
     if (date.getMonth() > currentDate.getMonth()) {
-      alert("cann.");
       return true;
     } else if (date.getMonth() == currentDate.getMonth()) {
+      // check date
       if (date.getDate() > currentDate.getDate()) {
-        alert("cannn.");
         return true;
       }
     }
-  } else {
-    //   ...
   }
-  alert("Date must be in the future." + date);
+  alert("Date must be in the future. \n You chosen: " + date);
   return false;
 }
 
 function chkExp() {
   var experience = document.getElementById("experience").value;
   experience.trim();
+
+  // check if empty
   if (experience.length > 0) {
-    alert("yes.");
     return true;
   }
   alert("Please fill in your experience." + experience);
