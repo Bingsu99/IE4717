e9ff74dd7cdf2861debda97585947e1818b22f2c
zhixinnnzx <185545x@gmail.com>
2023-10-24 01:52:03 +0800

    updated db + php conn to db

--- CaseStudy2/php/SalesReports.php
+++ CaseStudy2/php/SalesReports.php
@@ -12,11 +12,31 @@
 <body>
     <h1>Sales Reports</h1>
     <h2>Total dollar and quantity sales</h2>
+
+    <!--  -->
     <?php
+      // echo "selected: ". $report;
+      @ $db = new mysqli('localhost', 'root', '', 'javajam');
+      if (mysqli_connect_errno()) {
+        echo "Error: Could not connect to database.  Please try again later.";
+        exit;
+      }
+      // echo "<br> Connected successfully";
+      
+      $query = "select * from sales";
+      $result = $db->query($query);
+      if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+          echo "id: " . $row["sale_id"]. " - prodectID: " . $row["product_id"]. " - date " . $row["sale_date"]. 
+          " - qty " . $row["quantity_sold"] . " - amt " . $row["total_amount"] . "<br>";
+        }
+      } else {
+        echo "0 results";
+      }
+      $db->close();
 
-echo "selected: ". $report;
-
-?>
+    ?>
 </body>
 
 </html>
--- CaseStudy2/sql/javajam.sql
+++ CaseStudy2/sql/javajam.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 23, 2023 at 12:56 PM
+-- Generation Time: Oct 23, 2023 at 07:50 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -32,8 +32,20 @@ CREATE TABLE `coffeeproducts` (
   `product_name` varchar(100) DEFAULT NULL,
   `description` text DEFAULT NULL,
   `base_price` decimal(10,2) DEFAULT NULL,
-  `current_price` decimal(10,2) DEFAULT NULL
+  `current_price` decimal(10,2) DEFAULT NULL,
+  `shot_option` enum('single','double','Endless Cup') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `coffeeproducts`
+--
+
+INSERT INTO `coffeeproducts` (`product_id`, `product_name`, `description`, `base_price`, `current_price`, `shot_option`) VALUES
+(1, 'Just Java', 'Regular house blend, decaffinated coffee, or flavour of the day', 2.00, 2.00, 'Endless Cup'),
+(2, 'Cafe au Lait', 'House blended coffee infused into a smooth, steamed milk', 2.00, 2.00, 'single'),
+(3, 'Iced Cappucino', 'Sweetened espresso blended with icy-cold milk and served in a chilled glass', 4.75, 4.75, 'single'),
+(4, 'Cafe au Lait', 'House blended coffee infused into a smooth, steamed milk', 3.00, 3.00, 'double'),
+(5, 'Iced Cappucino', 'Sweetened espresso blended with icy-cold milk and served in a chilled glass', 5.75, 5.75, 'double');
 
 -- --------------------------------------------------------
 
@@ -48,6 +60,15 @@ CREATE TABLE `sales` (
   `quantity_sold` int(11) DEFAULT NULL,
   `total_amount` decimal(10,2) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `sales`
+--
+
+INSERT INTO `sales` (`sale_id`, `product_id`, `sale_date`, `quantity_sold`, `total_amount`) VALUES
+(1, 2, '2023-10-24', 1, 2.00),
+(2, 2, '2023-10-24', 1, 2.00),
+(3, 3, '2023-10-23', 2, 9.50);
 
 --
 -- Indexes for dumped tables
@@ -65,6 +86,22 @@ ALTER TABLE `coffeeproducts`
 ALTER TABLE `sales`
   ADD PRIMARY KEY (`sale_id`),
   ADD KEY `product_id` (`product_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `coffeeproducts`
+--
+ALTER TABLE `coffeeproducts`
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `sales`
+--
+ALTER TABLE `sales`
+  MODIFY `sale_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Constraints for dumped tables
