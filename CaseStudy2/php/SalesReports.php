93057016d4fcc4edb863617c44eee2be39e78934
zhixinnnzx <185545x@gmail.com>
2023-10-23 18:58:09 +0800

    added database

---
+++ CaseStudy2/php/SalesReports.php
@@ -0,0 +1,22 @@
+<?php
+  // create short variable names
+  $report = $_GET['report'];
+?>
+
+<html>
+
+<head>
+    <title>JavaJam - Sales Reports</title>
+</head>
+
+<body>
+    <h1>Sales Reports</h1>
+    <h2>Total dollar and quantity sales</h2>
+    <?php
+
+echo "selected: ". $report;
+
+?>
+</body>
+
+</html>
---
+++ CaseStudy2/sql/javajam.sql
@@ -0,0 +1,82 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 23, 2023 at 12:56 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `javajam`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `coffeeproducts`
+--
+
+CREATE TABLE `coffeeproducts` (
+  `product_id` int(11) NOT NULL,
+  `product_name` varchar(100) DEFAULT NULL,
+  `description` text DEFAULT NULL,
+  `base_price` decimal(10,2) DEFAULT NULL,
+  `current_price` decimal(10,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sales`
+--
+
+CREATE TABLE `sales` (
+  `sale_id` int(11) NOT NULL,
+  `product_id` int(11) DEFAULT NULL,
+  `sale_date` date DEFAULT NULL,
+  `quantity_sold` int(11) DEFAULT NULL,
+  `total_amount` decimal(10,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `coffeeproducts`
+--
+ALTER TABLE `coffeeproducts`
+  ADD PRIMARY KEY (`product_id`);
+
+--
+-- Indexes for table `sales`
+--
+ALTER TABLE `sales`
+  ADD PRIMARY KEY (`sale_id`),
+  ADD KEY `product_id` (`product_id`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `sales`
+--
+ALTER TABLE `sales`
+  ADD CONSTRAINT `sales_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `coffeeproducts` (`product_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
